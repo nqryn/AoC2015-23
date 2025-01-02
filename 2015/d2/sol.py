6e997ac3a272004780f1358778d4b0dd85f012d6
Adela <adela.vidrean@thredd.com>
2025-01-02 23:57:20 +0200

    V 2015.2 I did it

--- 2015/d2/sol.py
+++ 2015/d2/sol.py
@@ -1,20 +1,36 @@
 # Template
 import argparse
+import re
 
 
 def read_in(test_mode: bool):
 	f_name = 'f_test.in' if test_mode else 'f.in'
-	# Define vars here
-	x = 1
+	boxes = []
+	pattern = r'(?P<l>\d+)x(?P<w>\d+)x(?P<h>\d+)'
 	with open(f_name) as f:
 		for line in f:
-			# Remeber to line.strip() !!!!
-			x += int(line.strip())
-	return x
+			m = re.match(pattern, line)
+			if m:
+				l, w, h = m.group('l'), m.group('w'), m.group('h')
+				boxes.append((int(l), int(w), int(h)))
+	return boxes
 
 
-def solve(data):
-	return data + 1
+def solve(boxes):
+	wp = 0
+	for (l, w, h) in boxes:
+		sides = [l*w, l*h, w*h]
+		wp += 2 * sum(sides) + min(sides)
+	return wp
+
+
+def solve2(boxes):
+	ribbon = 0
+	for (l, w, h) in boxes:
+		wrap = 2 * ((l + w + h) - max(l, w, h))
+		bow = l * w * h
+		ribbon += wrap + bow
+	return ribbon
 
 
 if __name__ == '__main__':
@@ -23,6 +39,6 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	in_values = read_in(args.test)
-	res = solve(in_values)
+	res = solve2(in_values)
 	
 	print(f'Result: {res}')
